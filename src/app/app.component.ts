a3ed183a83fc8d92e5c575dcc864c4b47eb23bce
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-02 20:57:47 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  showPara = false;
+  count = 0;
+  logs = [];
+
+  onButtonClick() {
+    this.showPara = !this.showPara;
+    this.count++;
+    this.logs.push(this.count);
+  }
 }
